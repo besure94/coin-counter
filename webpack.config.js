c8a64f0f2032ce64cdfaba12879bcd878c25ae55
Brian Scherner <brianscherner@gmail.com>
2024-02-15 15:12:46 -0800

    Change name of root directory.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			verbose: true
 		}),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Coin Counter',
       template: './src/index.html',
       inject: 'body'
     }),

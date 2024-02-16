bba1d31fa8a2a54441123925aa66ff3cb95577ab
Brian Scherner <brianscherner@gmail.com>
2024-02-16 10:31:43 -0800

    Rewrite coinCounter function.

--- src/js/coinCounter.js
+++ src/js/coinCounter.js
@@ -1,4 +1,4 @@
-const coinCounter = function(amount) {
+const coinCounter = (amount) => {
   const validDecimal = /^\d+(\.\d{1,2})?$/;
   if(isNaN(amount) || amount <= 0) {
     throw "Invalid amount. Please enter a positive number.";

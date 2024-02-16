fab3900717de81a1a931b2d2f382bb6b1b0f55ab
Brian Scherner <brianscherner@gmail.com>
2024-02-16 09:14:14 -0800

    Modify first test to return an inputted number.

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -3,6 +3,7 @@ import coinCounter from './../src/js/coinCounter.js';
 describe('Coin counter function', () => {
 
   test('Should return an inputted number', () => {
-    expect(coinCounter(1)).toEqual(1);
+    const amount = 1;
+    expect(coinCounter(amount)).toEqual(1);
   });
 });

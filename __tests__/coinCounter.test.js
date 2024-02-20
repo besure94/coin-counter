7a45d3f2a3734977f7f9618c78f3725db37d7aa7
Brian Scherner <brianscherner@gmail.com>
2024-02-20 15:14:06 -0800

    Add functionality to count coins with recursive closure.

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -1,10 +1,37 @@
 import coinCounter from './../src/js/coinCounter.js';
 
+//refactor tests to work with recursive closure //
+
 describe('Coin counter function', () => {
 
   test('Function should not accept a non-numeric input of 0 or less.', () => {
     const amount = 0;
     expect(coinCounter(amount)).toBe("Amount should be a number greater than 0.");
   });
+
+  // test('Function should contain an inner function that can count quarters for an amount.', () => {
+  //   const amount = 1;
+  //   const coinAmounts = {
+  //     quarters: 0,
+  //     dimes: 0,
+  //     nickels: 0,
+  //     pennies: 0
+  //   };
+  //   const quarterCounter = coinCounter(amount, coinAmounts);
+  //   expect(typeof quarterCounter).toBe("function");
+  // });
+
+  // test('The quarterCounter function should count the number of quarters for the supplied amount.', () => {
+  //   const amount = 1;
+  //   const coinAmounts = {
+  //     quarters: 0,
+  //     dimes: 0,
+  //     nickels: 0,
+  //     pennies: 0
+  //   };
+  //   const result = coinCounter(amount, coinAmounts)();
+  //   expect(result.quarters).toEqual(4);
+
+  // });
 
 });
--- src/js/coinCounter.js
+++ src/js/coinCounter.js
@@ -1,7 +1,39 @@
-const coinCounter = (amount) => {
-  if (amount <= 0 || typeof amount !== "number") {
-    return "Amount should be a number greater than 0.";
+const coinCounter = (startingAmount) => {
+  const coinValues = {
+    quarter: 0.25,
+    dime: 0.10,
+    nickel: 0.05,
+    penny: 0.01
   }
+
+  const coinAmounts = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
+  }
+
+  function countCoins(amount, fn) {
+    if (amount <= 0 || typeof amount !== "number") {
+      return "Amount should be a number greater than 0.";
+    }
+    if (amount >= coinValues.quarter) {
+      coinAmounts.quarters++;
+      return fn(amount - coinValues.quarter, fn);
+    } else if (amount >= coinValues.dime) {
+      coinAmounts.dimes++;
+      return fn(amount - coinValues.dime, fn);
+    } else if (amount >= coinValues.nickel) {
+      coinAmounts.nickels++;
+      return fn(amount - coinValues.nickel, fn);
+    } else if (amount >= coinValues.penny) {
+      coinAmounts.pennies++;
+      return fn(amount - coinValues.penny, fn);
+    }
+    return coinAmounts;
+  }
+
+  return countCoins(startingAmount, countCoins);
 }
 
 export default coinCounter;
